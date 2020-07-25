9e4e631fa4b68d8357c6ac34feb89c36554f61e4
MasterProjectLC <dan.inafuku@hotmail.com>
2020-07-25 09:15:35 -0300

    debug

--- autropolis/settings.py
+++ autropolis/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
