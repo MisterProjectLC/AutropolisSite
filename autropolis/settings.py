77a036a2fd99fc0278486430064b7bcc0d1d7e04
MasterProjectLC <dan.inafuku@hotmail.com>
2020-07-25 09:18:57 -0300

    allowed hosts

--- autropolis/settings.py
+++ autropolis/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://autropolis-map.herokuapp.com/',
+    ]
 
 
 # Application definition
