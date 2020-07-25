51c2045e807ff574dcfc1449fb7ab01a8a49a69c
MasterProjectLC <dan.inafuku@hotmail.com>
2020-07-25 11:53:41 -0300

    bugfixes

--- autropolis/settings.py
+++ autropolis/settings.py
@@ -26,11 +26,12 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
                     'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+#DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
     'autropolis-map.herokuapp.com',
+    '127.0.0.1',
     ]
 
 
